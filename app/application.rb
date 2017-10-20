543e1684bf0ed90b96e8ebcb9e18db712260d69b
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-20 00:41:44 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path.match("/items/")
+      binding.pry
+      item = req.path.split("/").last
+      if Item.items.find{|t| t.name == item}
+        binding.pry
+        price = Item.items.find{|t| t.name == item}.price
+        resp.write price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
